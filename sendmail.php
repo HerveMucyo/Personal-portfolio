1de917be5e259672790866b00964d74f6fa00f25
Herve Mucyo <101579468+NKUBITOYIMANZI@users.noreply.github.com>
2023-05-28 00:24:34 +0200

    send email function added in php

--- sendmail.php
+++ sendmail.php
@@ -1,9 +1,14 @@
-<?php // get the form data $name = $_POST[‘name’]; $email = $_POST[‘email’]; $web = $_POST[‘web’]; $subject = $_POST[‘subject’]; $text = $_POST[‘text’];
+<?php $name = $_POST['name']; $email = $_POST['email']; $web = $_POST['web']; $subject = $_POST['subject']; $text = $_POST['text'];
 
-// set the email recipient $to = ‘your_email_address@example.com’;
+$to = 'mucyoh32@gmail.com';
 
-// set the email headers $headers = “From: $name <$email>\r\n”; $headers .= “Reply-To: $email\r\n”;
+$headers = 'From: $name <$email>\r\n'; $headers .= 'Reply-To: $email\r\n';
 
-// set the email body $message = “Name: $name\r\n”; $message .= “Email: $email\r\n”; $message .= “Web: $web\r\n”; $message .= “Subject: $subject\r\n”; $message .= “Text: $text\r\n”;
+ $message = 'Name: $name\r\n'; $message .= 'Email: $email\r\n'; $message .= 'Web: $web\r\n';$message .= 'Subject: $subject\r\n'; $message .= 'Text: $text\r\n';
 
-// send the email using mail() function if (mail($to, $subject, $message, $headers)) { echo “Email sent successfully!”; } else { echo “Email sending failed!”; } ?>
+if (mail($to, $subject, $message, $headers)) { 
+    echo 'Email sent successfully!'; } 
+    else 
+    { 
+        echo 'Email sending failed!'; 
+    } ?>
